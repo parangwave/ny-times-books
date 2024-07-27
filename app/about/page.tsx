89c6a8fe8588b709c21c4b697d83f908c81adc2e
Oh Jisu <parangwavetraveller@gmail.com>
2024-07-28 05:02:13 +0900

    feat: Add About page

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "About",
+};
+
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  );
+}
+
+export const runtime = "edge";
