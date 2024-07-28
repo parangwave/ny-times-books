fac07fdd86ee861f8a5e2d7b6d012f43b7cd60db
Oh Jisu <parangwavetraveller@gmail.com>
2024-07-29 02:35:03 +0900

    feat: fetch data in Home

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,19 @@
-export default function Home() {
-  return <h1>Home</h1>;
+import Link from "next/link";
+import { API_URL } from "../constants";
+
+export const metadata = {
+  title: "Home",
+};
+
+async function getGenres() {
+  return fetch(API_URL)
+    .then((resp) => resp.json())
+    .catch((error) => console.error(error));
+}
+
+export default async function Home() {
+  return (
+    <main>
+    </main>
+  );
 }
