1d2ecffa0351fa41690f7c4e48820229b87b7d52
Oh Jisu <parangwavetraveller@gmail.com>
2024-07-28 23:13:24 +0900

    feat: Add style in About

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,17 @@
+import styles from "../styles/about.module.css";
+
 export const metadata = {
   title: "About",
 };
 
 export default function About() {
   return (
-    <div>
-      <h1>About</h1>
+    <div className={styles.container}>
+      <h1>About This Project</h1>
+      <p>
+        This project was created to explore the New York Times Best Sellers
+        lists.
+      </p>
     </div>
   );
 }
