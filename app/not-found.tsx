2493ef85744815ca93fedbcedfb0a825b238595c
Oh Jisu <parangwavetraveller@gmail.com>
2024-07-29 02:34:14 +0900

    feat: Add not-found for home

---
+++ app/not-found.tsx
@@ -0,0 +1,14 @@
+// for unvalid url
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Not found",
+};
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1>Not found!</h1>
+    </div>
+  );
+}
